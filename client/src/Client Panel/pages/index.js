92a45a957fa3e20968cbd1c16a2ef6fbc111bf29
RHamzaZulfiqarJ <hamzazulfiqar123123@gmail.com>
2026-06-09 20:55:53 +0500

    Update website changes

--- client/src/Client Panel/pages/index.js
+++
@@ -1,3 +0,0 @@
-export { default as Contact } from './Contact Us/Contact'
-export { default as ClientDashboard } from './Dashboard/Home'
-export { default as ClientProjects } from './Your Projects/ClientProjects'
--- client/src/utils/index.js
+++
@@ -1,3 +0,0 @@
-export { default as Path } from './Path'
-export { default as Upload } from './Upload'
-export { default as Loader } from './Loader'
